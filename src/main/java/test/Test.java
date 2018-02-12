26ad71f8e795e85f8c31fb5d70e58da9f581ea88
jojoldu <jojoldu@gmail.com>
2018-02-12 20:17:49 +0900

    [ST-1261] [테스트] IntelliJ 연동 테스트

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 2. 12.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        String a="a";
+    }
+}
